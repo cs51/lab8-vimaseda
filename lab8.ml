c79a2644f2f055f9e277ddadbef70802d792e6f7
Victoria Maseda <vimaseda@college.harvard.edu>
2018-04-12 15:38:18 -0400

    s

--- lab8.ml
+++ lab8.ml
@@ -138,7 +138,7 @@ one. If there is no listener with that id, do nothing.
 Exercise 3: Write fire_event, which will execute all event handlers
 listening for the event.
 ......................................................................*)
-            
+          
   let fire_event (evt : 'a event) (arg : 'a) : unit =
     List.fold_left (fun _ x -> x.action arg) () !evt ;;
 
